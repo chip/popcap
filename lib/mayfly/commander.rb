9148bce25e66929ece1a03668e812f5f8cb9535d
CulleyS <culley.smith@gmail.com>
2013-01-30 22:45:55 -0600

    add Commander class for executing shell commands

---
+++ lib/mayfly/commander.rb
@@ -0,0 +1,68 @@
+require 'open3'
+
+module Mayfly
+  # Internal: This is a wrapper for the Open3 Ruby Standard Library.
+  #
+  # Examples
+  #
+  #   options = %W{ls} + %W{-lh}
+  #   Commander.new(options).execute.output
+  #   # => <directory contents>
+  #
+  class Commander
+    # Internal: Initialize
+    #
+    # args - Arguments should be escaped with an interpolated literal.
+    #
+    def initialize(*args)
+      raise(ArgumentError, error_message) if args.empty?
+      @command = args.inject(%W{}) { |command, arg| command << arg }
+      @executed = []
+    end
+
+    # Internal: Execute the command using Open3.capture3.
+    #
+    # execute - It will return an instance of Commander, in order
+    # to chain methods.
+    #
+    def execute
+      @executed = Open3.capture3(*@command)
+      self
+    end
+
+    # Internal: Open3.capture3 returns an array of three elements.
+    #
+    # stdout - The first element returned is stdout.
+    #
+    def stdout
+      @executed[0]
+    end
+
+    # Internal: Open3.capture3 returns an array of three elements.
+    #
+    # stderr - The second element returned is stderr.
+    #
+    def stderr
+      @executed[1]
+    end
+
+    # Internal: Open3.capture3 returns an array of three elements.
+    #
+    # status - The third element returned is status.  Status can
+    # have a 'success?' of true or false.
+    #
+    # Examples
+    # 
+    #   self.status.success?
+    #   # => true
+    #
+    def status
+      @executed[2]
+    end
+
+    private
+    def error_message
+      'Cannot run command with no args.'
+    end
+  end
+end
---
+++ spec/lib/mayfly/commander_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require 'mayfly/commander'
+
+module Mayfly
+  describe Commander do
+    let(:commander) { Commander.new(%W{ls},%W{-l},%W{/tmp}) }
+
+    context "#new" do
+      it 'raises an error if no arguments' do
+        expect{Commander.new}.to raise_error(ArgumentError)
+      end
+
+      it 'sets @executed to empty' do
+        expect(commander.instance_variable_get('@executed')).to be_empty
+      end
+
+      it 'escapes @command' do
+        expect(commander.instance_variable_get('@command')).
+          to eq([['ls'],['-l'],['/tmp']])
+      end
+    end
+
+    context '#execute' do
+      it 'executes a shell command' do
+        Open3.should_receive(:capture3).with(['ls'],['-l'],['/tmp'])
+        commander.execute
+      end
+
+      it 'returns itself' do
+        Open3.should_receive(:capture3).with('blah')
+        command = Commander.new('blah')
+        expect(command.execute).to be_a(Commander)
+      end
+    end
+
+    it 'returns stdout' do
+      Open3.should_receive(:capture3).with('blah') { ['foo',nil,nil] }
+      command = Commander.new('blah').execute
+      expect(command.stdout).to eq('foo')
+    end
+
+    it 'returns stderr' do
+      Open3.should_receive(:capture3).with('blah') { [nil,'bar',nil] }
+      command = Commander.new('blah').execute
+      expect(command.stderr).to eq('bar')
+    end
+    
+    context '#status' do
+      it 'is true if it succeeds' do
+        succeeded = double('status', success?: true)
+        Open3.should_receive(:capture3).with('blah') { [nil,nil,succeeded] }
+        command = Commander.new('blah').execute
+        expect(command.status.success?).to be_true
+      end
+
+      it 'is false if it fails' do
+        failed = double('status', success?: false)
+        Open3.should_receive(:capture3).with('blah') { [nil,nil,failed] }
+        command = Commander.new('blah').execute
+        expect(command.status.success?).to be_false
+      end
+    end
+  end
+end
