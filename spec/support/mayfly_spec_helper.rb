7ee6045ed6f9c90dca553377a8a5d7057cd204be
CulleyS <culley.smith@gmail.com>
2013-01-30 22:46:33 -0600

    add example ffmpeg output to MayflySpecHelper module

--- spec/support/mayfly_spec_helper.rb
+++ spec/support/mayfly_spec_helper.rb
@@ -1,6 +1,24 @@
 module MayflySpecHelper
   class << self
     def raw_tags
+      <<-EOF.gsub(/^\s+/,'')
+        [FORMAT]
+        filename=spec/support/sample.flac
+        nb_streams=1
+        format_name=flac
+        format_long_name=raw FLAC
+        start_time=N/A
+        duration=1.000000
+        size=18291
+        bit_rate=146328
+        TAG:GENRE=Sample Genre
+        TAG:track=01
+        TAG:ALBUM=Sample Album
+        TAG:DATE=2012
+        TAG:TITLE=Sample Title
+        TAG:ARTIST=Sample Artist
+        [/FORMAT]
+      EOF
     end
   end
 end
