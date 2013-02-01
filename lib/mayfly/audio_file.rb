456a6aa03ea5234c33d03037fa3a503fc2f5f67c
CulleyS <culley.smith@gmail.com>
2013-01-31 20:44:00 -0600

    add #raw_tags to AudioFile which returns raw metadata output from FFmpeg

--- lib/mayfly/audio_file.rb
+++ lib/mayfly/audio_file.rb
@@ -1,10 +1,31 @@
+require 'mayfly/ffmpeg'
+
 module Mayfly
+  # Public: This is a class for managing audio files on the filesystem.
+  # It is used to read & write metadata tags, convert between audio formats,
+  # and manage a file on the filesystem using standard UNIX file commands.
+  #
+  # Examples
+  #
+  #   filepath = 'spec/support/sample.flac'
+  #   af = AudioFile.new(filepath)
+  #
+  #
   class AudioFile
+
+    # Public: Initialize
+    #
+    # filepath - Requires a valid filepath to a file on the local filesystem.
+    #
     def initialize(filepath)
       @filepath = filepath
     end
 
+    # Public: raw_tags
+    # Returns the raw output of FFProbe's show_format option.
+    #
     def raw_tags
+      FFmpeg.new(@filepath).read_tags
     end
   end
 end
