fca7ef206bd68b92776287385da7ed808afbf24b
CulleyS <culley.smith@gmail.com>
2013-02-01 23:03:25 -0600

    add Taggable module to provide behavior for building structures from FFmpeg raw output

---
+++ lib/mayfly/taggable.rb
@@ -0,0 +1,41 @@
+require 'mayfly/tag_line'
+
+module Mayfly
+  # Internal: This module is included in anything with a #raw_tags
+  # attribute.  It is used to parse and build tags from FFmpeg raw output.
+  #
+  module Taggable
+
+    # Internal: This method builds a sanitized hash from #raw_tags
+    #
+    # Examples
+    #   class SomeClass
+    #     def raw_tags
+    #     end
+    #   end
+    #
+    #   klass = SomeClass.new
+    #   klass.to_hash
+    #   # => 
+    #       { filename: 'spec/support/sample.flac',
+    #         format_name: 'flac',
+    #         duration: '1.000000',
+    #         filesize: '18291',
+    #         bit_rate: '146328',
+    #         genre: 'Sample Genre',
+    #         track: '01',
+    #         album: 'Sample Album',
+    #         date: '2012',
+    #         title: 'Sample Title',
+    #         artist: 'Sample Artist' }
+    #
+    def to_hash
+      lines.inject({}) {|hash,line| hash.merge(TagLine.new(line)).to_hash }
+    end
+
+    private
+    def lines
+      self.raw_tags.split("\n")
+    end
+  end
+end
---
+++ spec/lib/mayfly/taggable_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'support/mayfly_spec_helper'
+require 'mayfly/taggable'
+
+module Mayfly
+  describe Taggable do
+    class SomeClass
+      include Taggable
+      def raw_tags
+        MayflySpecHelper.raw_tags
+      end
+    end
+
+    let(:sc) { SomeClass.new }
+
+    it 'has #raw_tags' do
+      expect(sc).to respond_to(:raw_tags)
+    end
+
+    context '#to_hash' do
+      it 'builds a sanitized hash from FFmpeg raw_tags' do
+        expect(sc.to_hash).to eq MayflySpecHelper.to_hash
+      end
+    end
+  end
+end
