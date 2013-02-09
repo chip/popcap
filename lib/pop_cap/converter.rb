b3b9df05941afd26ba38191e11d36e2c1f78ea9f
CulleyS <culley.smith@gmail.com>
2013-02-08 21:09:37 -0600

    refactor error raising from Converter class

--- lib/pop_cap/converter.rb
+++ lib/pop_cap/converter.rb
@@ -3,12 +3,6 @@ module PopCap
   # to the specified output format. The module is included in FFmpeg.
   #
   module Converter
-    ::BitRateConversionError = Class.new(StandardError)
-    ::FormatConversionError = Class.new(StandardError)
-
-    ::VALIDBITRATES = %q(64 128 160 192 256 320)
-    ::VALIDFORMATS = %q(aac flac m4a mp3 ogg wav)
-
     # Internal: This method takes an input format & optional bitrate
     # in order to supply the ffmpeg command used to convert.
     #
@@ -22,29 +16,16 @@ module PopCap
     end
 
     private
-    def bitrate_error_message
-      'An invalid bitrate was supplied.  Try 64, 128, 160, 192, 256, 320.'
-    end
-
     def bitrate_options
-      return %W{} unless use_bitrate?
       %W{-ab #{@bitrate}k}
-    end
-
-    def format_error_message
-      'An invalid format was supplied.  Try aac, flac, m4a, mp3, ogg, wav.'
     end
 
     def input_path
       %W{ffmpeg -i #{self.filepath}}
     end
 
-    def output_filename
-      self.filepath.sub(%r([^.]+\z),@format)
-    end
-
     def output_path
-      %W{#{output_filename}}
+      %W{#{self.filepath.sub(%r([^.]+\z),@format)}}
     end
 
     def strict_mode
@@ -52,17 +33,7 @@ module PopCap
       %W{-strict -2}
     end
 
-    def use_bitrate?
-      unless VALIDBITRATES.include?(@bitrate.to_s)
-        raise(BitRateConversionError, bitrate_error_message)
-      end
-      @format == 'mp3'
-    end
-
     def use_strict_mode?
-      unless VALIDFORMATS.include?(@format)
-        raise(FormatConversionError, format_error_message)
-      end
       @format == 'm4a'
     end
   end
--- spec/lib/pop_cap/converter_spec.rb
+++ spec/lib/pop_cap/converter_spec.rb
@@ -10,6 +10,7 @@ module PopCap
       end
     end
 
+    let(:bitrate) { %W{-ab 192k}}
     let(:filepath) { 'path/to/file.flac' }
     let(:fk) { FakeClass.new }
     let(:input) { %W{ffmpeg -i #{filepath}} }
@@ -18,16 +19,16 @@ module PopCap
       let(:output_path) { %W{path/to/file.ogg} }
 
       it 'builds a command' do
-        expect(fk.convert('ogg')).to eq(input + output_path)
+        expect(fk.convert('ogg')).to eq(input + bitrate + output_path)
       end
 
       context 'format' do
         it 'handles symbol' do
-          expect(fk.convert(:ogg)).to eq(input + output_path)
+          expect(fk.convert(:ogg)).to eq(input + bitrate + output_path)
         end
 
         it 'is case insenstive' do
-          expect(fk.convert('OGG')).to eq(input + output_path)
+          expect(fk.convert('OGG')).to eq(input + bitrate + output_path)
         end
       end
 
@@ -46,31 +47,19 @@ module PopCap
           expect(fk.convert(:mp3, '128')).
             to eq(input + %W{-ab 128k} + output_path)
         end
-
-        it 'ignores bitrate if format is not mp3' do
-          expect(fk.convert(:ogg, 128)).to eq(input + %W{path/to/file.ogg})
-        end
       end
 
       context 'm4a' do
         let(:output_path) { %W{path/to/file.m4a} }
 
         it 'uses strict mode' do
-          expect(fk.convert(:m4a)).to eq(input + %W{-strict -2} + output_path)
+          expect(fk.convert(:m4a)).
+            to eq(input + %W{-strict -2} + bitrate + output_path)
         end
 
         it 'ignores strict mode if not m4a' do
-          expect(fk.convert(:ogg)).to eq(input + %W{path/to/file.ogg})
-        end
-      end
-
-      context 'errors' do
-        it 'raises BitRateConversionError if invalid bitrate' do
-          expect{fk.convert(:mp3, 'foo')}.to raise_error(BitRateConversionError)
-        end
-
-        it 'raises FormatConversionError if invalid format' do
-          expect{fk.convert(:foo)}.to raise_error(FormatConversionError)
+          expect(fk.convert(:ogg)).
+            to eq(input + bitrate + %W{path/to/file.ogg})
         end
       end
     end
