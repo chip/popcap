8072a75dde7f531aeb99cf59c3ceede19cb17116
CulleyS <culley.smith@gmail.com>
2013-03-02 11:39:35 -0600

    add Formatter .subclasses & .subclasses_demodulized to reveal information about all subclasses of a Formatter

--- lib/pop_cap/formatter.rb
+++ lib/pop_cap/formatter.rb
@@ -2,27 +2,52 @@ module PopCap
   # Public: This is a super class for all formatter classes.
   # It establishes the default behavior of formatter classes.
   #
-  class Formatter
-    attr_reader :value, :options
+  module Formatters
+    class Formatter
+      attr_reader :value, :options
 
-    # Public: This class is constructed with a value & options hash.
-    #
-    # value - The value to be formatted.
-    # options - An options hash.
-    #
-    def initialize(value, options={})
-      @value, @options = value, options
-    end
+      # Public: This class is constructed with a value & options hash.
+      #
+      # value - The value to be formatted.
+      # options - An options hash.
+      #
+      def initialize(value, options={})
+        @value, @options = value, options
+      end
 
-    # Public: This method contains the handles the formating.
-    #
-    def format
-    end
+      # Public: This method contains the handles the formating.
+      #
+      def format
+      end
 
-    # Public: A class method for convenience calling of #format.
-    #
-    def self.format(value, options={})
-      new(value, options).format
+      # Public: A class method for convenience calling of #format.
+      #
+      def self.format(value, options={})
+        new(value, options).format
+      end
+
+      # Public: This returns an array of all subclasses.
+      #
+      def self.subclasses
+        ObjectSpace.each_object(Class).map do |klass| 
+          klass if is_a_subclass?(klass)
+        end.uniq.compact
+      end
+
+      # Public: This returns an array of all subclasses "demodulized."
+      #
+      def self.subclasses_demodulized
+        subclasses.map { |klass| demodulize(klass) }
+      end
+
+      private
+      def self.demodulize(klass)
+        klass.to_s.split('::').last
+      end
+
+      def self.is_a_subclass?(klass)
+        klass.superclass && klass.superclass.name == self.name
+      end
     end
   end
 end
--- spec/lib/pop_cap/formatter_spec.rb
+++ spec/lib/pop_cap/formatter_spec.rb
@@ -2,32 +2,61 @@ require 'spec_helper'
 require 'pop_cap/formatter'
 
 module PopCap
-  describe Formatter do
-    class Taggy < Formatter
-      def format; 'baz'; end
-    end
-
-    let(:taggy) { Taggy.new('foo', {option: 'bar'}) }
-
-    describe '.format' do
-      it 'wraps #format in a class method' do
-        klass = Taggy.format('foo', {option: 'bar'})
-        instance = taggy.format
-        expect(klass).to eq instance
-      end
-    end
-
-    describe '#format' do
-      it { expect(taggy).to respond_to(:format) }
-    end
-
-    describe '#new' do
-      it 'has a getter for value' do
-        expect(taggy.value).to eq 'foo'
+  module Formatters
+    describe Formatter do
+      class TagClass < Formatter
+        def format; 'baz'; end
       end
 
-      it 'has a getter for options' do
-        expect(taggy.options).to eq({option: 'bar'})
+      describe '.format' do
+        let(:taggy) { Formatters::TagClass.new('foo', {option: 'bar'}) }
+
+        it 'wraps #format in a class method' do
+          klass = Formatters::TagClass.format('foo', {option: 'bar'})
+          instance = taggy.format
+          expect(klass).to eq instance
+        end
+      end
+
+      describe '.subclasses' do
+        it 'returns a list of all subclasses in the ObjectSpace' do
+          expect(Formatter.subclasses).to include(PopCap::Formatters::TagClass)
+        end
+
+        it 'includes the subclass only once' do
+          unique = Formatter.subclasses.select do |cls| 
+            cls == PopCap::Formatters::TagClass
+          end
+          expect(unique.size).to eq 1
+        end
+
+        it 'contains no nils' do
+          expect(Formatter.subclasses).not_to include(nil)
+        end
+      end
+
+      describe '.subclasses_demodulized' do
+        it 'returns of the subclasses with their names demodulized' do
+          expect(Formatter.subclasses_demodulized).to include(%w(TagClass))
+        end
+      end
+
+      describe '#format' do
+        let(:taggy) { Formatters::TagClass.new('foo', {option: 'bar'}) }
+
+        it { expect(taggy).to respond_to(:format) }
+      end
+
+      describe '#new' do
+        let(:taggy) { Formatters::TagClass.new('foo', {option: 'bar'}) }
+
+        it 'has a getter for value' do
+          expect(taggy.value).to eq 'foo'
+        end
+
+        it 'has a getter for options' do
+          expect(taggy.options).to eq({option: 'bar'})
+        end
       end
     end
   end
